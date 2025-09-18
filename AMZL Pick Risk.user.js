88ac7a4697f62980f67ed487f3cf145eb531c9ec
jackbehm-amzl <behmrj@gmail.com>
2025-09-18 10:06:19 -0400

    Update AMZL Pick Risk.user.js

--- AMZL Pick Risk.user.js
+++ AMZL Pick Risk.user.js
@@ -7,8 +7,8 @@
 // @match        https://logistics.amazon.com/station/dashboard/pick*
 // @match        https://ui.*.last-mile.amazon.dev/*
 // @match        https://*.last-mile.amazon.dev/*
-// @updateURL    https://raw.githubusercontent.com/jackbehm-amzl/amzl-pick-risk/main/AMZL%20Pick%20Risk.user.js
-// @downloadURL  https://raw.githubusercontent.com/jackbehm-amzl/amzl-pick-risk/main/AMZL%20Pick%20Risk.user.js
+// @updateURL    https://raw.githubusercontent.com/jackbehm-amzl/amzl-pick-risk/main/AMZL%20Pick%20Risk-4.0.user.js
+// @downloadURL  https://raw.githubusercontent.com/jackbehm-amzl/amzl-pick-risk/main/AMZL%20Pick%20Risk-4.0.user.js
 // @run-at       document-idle
 // @grant        none
 // ==/UserScript==
